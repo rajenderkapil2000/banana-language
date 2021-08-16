2ff2703db941b26c035de05b62d55c7366f998ee
rajenderkapil2000 <rajenderkapil2000@gmail.com>
2021-08-16 20:49:31 +0530

    updated

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var output = document.querySelector("#output");
+var outputDiv = document.querySelector("#output");
 
-function clickHandler(){
-  output.innerText= "output " + txtInput.value
+var serverURL = "https://api.funtranslations.com/translate/yoda.json"
 
+function getTranslationURL(text) {
+  return serverURL + "?" + "text" + text
 }
 
+function errorHandler(error) {
+  console.log("error occured", error);
+}
+
+function clickHandler() {
+  var inputText = txtInput.value;
+
+  fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+
 btnTranslate.addEventListener("click", clickHandler)
-
-
-"https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=i am iron man"
