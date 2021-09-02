d4ac56d89fb868eeee5d51ac5a08bee793661abd
rajenderkapil2000 <rajenderkapil2000@gmail.com>
2021-09-02 13:23:07 +0530

    added some style

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(input) {
-  return serverURL + "?" + "text" + input
+  return serverURL + "?" + "text=" + input
 }
 
 function errorHandler(error) {
